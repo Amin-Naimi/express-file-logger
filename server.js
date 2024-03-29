6d6ed167b62e312f4fac424ff6d1e3f3219b40c7
Mohamed <ahnbi32@gmail.com>
2024-03-29 13:05:15 +0100

    -3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const xmlparser = require('express-xml-bodyparser');
 const xmlbuilder = require('xmlbuilder');
+const ExcelJS = require('exceljs');
 
 const app = express();
 const port = 3000
@@ -11,12 +12,35 @@ app.use(xmlparser());
 app.post('/xml', function (req, res) {
     let xmlData = req.body;
     let xmlString = xmlbuilder.create(xmlData).end({ pretty: true});
-    fs.appendFile('output.xml', xmlString, function(err) {
+    fs.appendFile('../xml_files/output.xml', xmlString, function(err) {
         if(err) {
             console.log(err);
-            res.status(500).send('Erreur lors de l\'écriture du fichier');
+            res.status(500).send('Erreur lors de l\'écriture du fichier XML');
         } else {
-            res.status(200).send('Données XML reçues et stockées avec succès');
+            let jsonData = JSON.stringify(xmlString)
+            console.log(jsonData);
+            // Création d'un nouveau workbook
+            let workbook = new ExcelJS.Workbook();
+            let worksheet = workbook.addWorksheet('Données XML');
+
+            // Ajout des données dans le worksheet
+            // Ici, je suppose que xmlData est un objet avec des clés et des valeurs
+            let rowNumber = 1;
+            for (let key in xmlData) {
+                worksheet.getCell(`A${rowNumber}`).value = key;
+                worksheet.getCell(`B${rowNumber}`).value = xmlData[key];
+                rowNumber++;
+            }
+
+            // Sauvegarde du fichier Excel
+            workbook.xlsx.writeFile('../excel_files/output.xlsx')
+            .then(function() {
+                res.status(200).send('Données XML reçues et stockées avec succès dans les fichiers XML et Excel');
+            })
+            .catch(function(err) {
+                console.log(err);
+                res.status(500).send('Erreur lors de l\'écriture du fichier Excel');
+            });
         }
     });
 });
