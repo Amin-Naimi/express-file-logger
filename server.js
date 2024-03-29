9423a96d17a74b7de3a903a36ac9494ee1a082f1
Mohamed <ahnbi32@gmail.com>
2024-03-29 01:46:49 +0100

    update code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 8090;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, '../uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
